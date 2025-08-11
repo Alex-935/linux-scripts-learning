a35522aee1663a57793b4ef3798c8d06da1516a6
Alex Potter <69348601+Alex-935@users.noreply.github.com>
2025-08-11 13:44:53 +0100

    Create greet.sh

---
+++ greet.sh
@@ -0,0 +1,14 @@
+# This script accepts the users name and prints a greeting message
+
+# Print prompt
+echo -n "Enter your name :"	  	
+
+# get name from the user
+read name				
+
+# Print message and entered name
+echo "Welcome $name"
+
+# Prints on a single line. Hence the usage of \'-n\'
+echo -n "Congratulations!"
+echo " The script was successful"
