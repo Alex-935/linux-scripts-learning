8716bfde12d2a43eb98347618711859a1c1d5be4
Alex Potter <69348601+Alex-935@users.noreply.github.com>
2025-08-11 20:26:28 +0100

    Update fileToArray.sh
    Update fileToArray.sh

--- fileToArray.sh
+++ fileToArray.sh
@@ -8,7 +8,18 @@ column1=($(cut -d "," -f 1 $csv))
 column2=($(cut -d "," -f 2 $csv))
 column3=($(cut -d "," -f 3 $csv))
 
+#difference between 2nd and 3rd column
+#define column with header
+column4=(column_3)
+
+#for each row, append the difference between row 3 and 2 to column4
+for ((i=1; i<${#column1[@]}; i++)); do
+        column4[$i]+=$((column3[$i]-column2[$i]))
+done
+
 # print columns
 echo "${column1[@]}"
 echo "${column2[@]}"
 echo "${column3[@]}"
+echo "${column4[@]}"
+
