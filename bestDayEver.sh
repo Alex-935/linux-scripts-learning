3e48efe60dd956aa112a3c499d910edd51910f47
Alex Potter <69348601+Alex-935@users.noreply.github.com>
2025-08-11 17:53:14 +0100

    bestDayEver.sh

--- bestDayEver.sh
+++ bestDayEver.sh
@@ -6,14 +6,18 @@
 
 
 #Positional parameter, will take the first input after the run command and use that as the var value
-firstName=$1
+#firstName=$1
+compliment=$1
 
+#Used to make a variable the output of a command
+user=$(whoami)
+date=$(date +%A)
 
 #add delay between lines
 sleep 1
 
-echo "Hello $firstName."
-
+echo "Hello $user."
 sleep 1
-
-echo "You're looking good today $firstName."
+echo "You're looking good today on this fine $date."
+sleep 1
+echo "You have the best $compliment I've ever seen"
