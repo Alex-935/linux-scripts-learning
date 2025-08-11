f4a1eefd5f9316350be53d33d00a41d9b6d17225
Alex Potter <69348601+Alex-935@users.noreply.github.com>
2025-08-11 17:39:53 +0100

    Updated bestDayEver.sh

--- bestDayEver.sh
+++ bestDayEver.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 
 #take in user's name
-echo -n "Hello, what's your name: "
-read firstName
+#echo -n "Hello, what's your name: "
+#read firstName
+
+
+#Positional parameter, will take the first input after the run command and use that as the var value
+firstName=$1
+
 
 #add delay between lines
 sleep 1
