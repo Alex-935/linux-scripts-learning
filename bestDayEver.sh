b5471e668ed89709f7f97684aa8d4eb9c65d9fb9
Alex Potter <69348601+Alex-935@users.noreply.github.com>
2025-08-11 17:31:50 +0100

    Add files via upload

---
+++ bestDayEver.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+#take in user's name
+echo -n "Hello, what's your name: "
+read firstName
+
+#add delay between lines
+sleep 1
+
+echo "Hello $firstName."
+
+sleep 1
+
+echo "You're looking good today $firstName."
