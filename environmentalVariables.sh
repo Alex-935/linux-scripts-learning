d36e1424d8cb263c5735eddb8350492a48c20d8a
Alex Potter <69348601+Alex-935@users.noreply.github.com>
2025-08-16 11:18:11 +0100

    environmentalVariables.sh

---
+++ environmentalVariables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "$PWD, $SHELL, $USER, $HOSTNAME"
+
+
+echo "Your random number between 0 and 32767 is: $RANDOM"
+
+
+#Make your own environmental variables
+#twitter="Elon Musk"
+#export twitter
+#echo $twitter
+
+#to make it permanent and available every time you log in
+#edit the .bashrc file and add export twitter="Elon Musk"
